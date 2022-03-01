1c45cc4147c9c5faa679d35f18f95200770d6ce6
NGG-kang <skarndrkd1222@gmail.com>
2022-03-01 11:19:31 +0900

    Fix: redis connection error

--- company_review/config/settings/base.py
+++ company_review/config/settings/base.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://host.docker.internal",
+        "LOCATION": "redis://@redis:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
