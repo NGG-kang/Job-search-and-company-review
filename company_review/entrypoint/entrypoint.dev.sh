32e3d1c4af740dac1df17795d86cfe951e08cb7b
NGG-kang <skarndrkd1222@gmail.com>
2022-03-01 10:49:21 +0900

    Feat: entrypoint celery modify

--- company_review/entrypoint/entrypoint.dev.sh
+++ company_review/entrypoint/entrypoint.dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-celery -A celeries.celery worker -B -E -l INFO &
+celery -A celeries.celery worker -B -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
 
 exec "$@"
--- company_review/entrypoint/entrypoint.prod.sh
+++ company_review/entrypoint/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-celery -A celeries.celery worker -B -E -l INFO &
+celery -A celeries.celery worker -B -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 python manage.py collectstatic --noinput
 
