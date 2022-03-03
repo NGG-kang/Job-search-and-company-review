2f83e42320d228591e6b91bd5051e18a1c0f5e53
NGG-kang <skarndrkd1222@gmail.com>
2022-03-03 23:08:28 +0900

    Feat: crawling company refactoring

--- company_review/celeries/celery.py
+++ company_review/celeries/celery.py
@@ -17,6 +17,11 @@ sys.path.append(os.path.dirname(BASE_DIR))
 django.setup()
 
 from crawling.search_jobs.search import search_and_save
+from celeries.tasks import (
+    get_kreditjob_company,
+    get_saramin_company,
+    get_jobplanet_company,
+)
 
 
 @app.on_after_configure.connect
@@ -24,3 +29,10 @@ def setup_periodic_tasks(sender, **kwrags):
     sender.add_periodic_task(
         300.0, search_and_save.s(kwrags["q"], kwrags["celery"]), name="django search"
     )
+
+
+@app.task(bind=True)
+def get_company_info(self, name):
+    get_jobplanet_company(name)
+    get_saramin_company(name)
+    get_kreditjob_company(name)
--- company_review/celeries/tasks.py
+++ company_review/celeries/tasks.py
@@ -3,8 +3,8 @@ import sys
 import django
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(os.path.dirname(BASE_DIR))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# sys.path.append(os.path.dirname(BASE_DIR))
 from celery import shared_task
 from crawling.models import JobPlanet, KreditJob, Saramin
 import json
@@ -14,29 +14,28 @@ import requests
 import re
 
 
-def find_content(soup):
-    company_name = (
-        soup.find("div", {"class": "company_info_box"})
-        .find("a")
-        .text.replace("(주)", "")
-    )
-    data_json = soup.find("ul", {"class": "basic_info_more"})
-    company_info = dict()
-    for data in data_json:
-        header = data.find("dt")
-        content = data.find("dd")
-        if header == -1:
-            continue
-        company_info[header.text.strip()] = content.text.strip()
-    return (company_name, company_info)
-
-
-@shared_task
-def jobplanet(name):
+def get_jobplanet_company(name):
     print(
         "---------------------------------JOBPLNET----------------------------------------"
     )
-    for _ in range(5):
+
+    def find_content(soup):
+        company_name = (
+            soup.find("div", {"class": "company_info_box"})
+            .find("a")
+            .text.replace("(주)", "")
+        )
+        data_json = soup.find("ul", {"class": "basic_info_more"})
+        company_info = dict()
+        for data in data_json:
+            header = data.find("dt")
+            content = data.find("dd")
+            if header == -1:
+                continue
+            company_info[header.text.strip()] = content.text.strip()
+        return (company_name, company_info)
+
+    for _ in range(2):
         try:
             search_url = f"https://www.jobplanet.co.kr/autocomplete/autocomplete/suggest.json?term={name}"
             html = requests.get(search_url, allow_redirects=False, proxies=proxies)
@@ -74,16 +73,13 @@ def jobplanet(name):
             break
         except Exception as e:
             print(e)
-        finally:
-            kreditjob_crawling(company=company_name)
 
 
-@shared_task
 def get_saramin_company(name):
     page = 1
-    while True:
+    for _ in range(2):
         resq = requests.get(
-            f"https://www.saramin.co.kr/zf_user/search/company?searchword={name}&page={page}&searchType=search&pageCount=30&mainSearch=n",
+            f"https://www.saramin.co.kr/zf_user/search/company?searchword={name}&page={page}",
             proxies=proxies,
         )
         soup = BeautifulSoup(resq.content, "lxml")
@@ -119,7 +115,7 @@ def get_saramin_company(name):
         page += 1
 
 
-def kreditjob_crawling(company):
+def get_kreditjob_company(company):
     print(
         "---------------------------------KREDITJOB----------------------------------------"
     )
@@ -135,105 +131,71 @@ def kreditjob_crawling(company):
 
     print("-----------------검색---------------------")
     for search_company in search_response:
-        print(search_company)
-        CMPN_NM = search_company["CMPN_NM"]
-        WKP_ADRS = search_company["WKP_ADRS"]
-        PK_NM_HASH = search_company["PK_NM_HASH"]
         try:
-            k = KreditJob.objects.get(company_pk=PK_NM_HASH)
-            print("skip")
-            break
-            k.name = CMPN_NM
-            k.address = WKP_ADRS
-            k.company_base_content = company_base_content
-            k.company_info_data = company_info_data
-            k.company_jobdam = company_jobdam
-            k.save()
-        except KreditJob.DoesNotExist:
-            company_base_content = json.loads(
-                requests.get(
-                    f"https://www.wanted.co.kr/api/v1/company_briefs?kreditjob_pk={PK_NM_HASH}",
-                    proxies=proxies,
-                ).content
-            )
-            # POST
-            # 기업 연봉포함 정보
-            # data
-            data = {"PK_NM_HASH": PK_NM_HASH}
-            company_info_data = json.loads(
-                requests.post(
-                    "https://www.kreditjob.com/api/company/companyPage",
-                    data=data,
-                    proxies=proxies,
-                ).content
-            )
-            # POST
-            # 기업 잡담
-            data = {"PK_NM_HASH": ["PK_NM_HASH", PK_NM_HASH]}
-            company_jobdam = json.loads(
-                requests.post(
-                    "https://www.kreditjob.com/api/jobdom/multiFilter/1/10",
-                    data=data,
-                    proxies=proxies,
-                ).content
-            )
-            # print("-----------------기업기본정보---------------------")
-            # print(company_base_content)
-            # print("-----------------기업연봉포함정보---------------------")
-            # print(company_info_data)
-            # print("-----------------기업잡담---------------------")
-            # print(company_jobdam)
-
-            print("-----------------기업기본정보---------------------")
-            print(company_base_content)
-            KreditJob(
-                name=CMPN_NM,
-                address=WKP_ADRS,
-                company_pk=PK_NM_HASH,
-                company_base_content=company_base_content,
-                company_info_data=company_info_data,
-                company_jobdam=company_jobdam,
-            ).save()
-
-
-@shared_task
-def get_saramin_company(name):
-    page = 1
-    while True:
-        resq = requests.get(
-            f"https://www.saramin.co.kr/zf_user/search/company?searchword={name}&page={page}&searchType=search&pageCount=30&mainSearch=n#company_info",
-            proxies=proxies,
-        )
-        soup = BeautifulSoup(resq.content, "lxml")
-        search_list = soup.find_all("div", class_="item_corp")
-        if not search_list:
-            break
-        while True:
+            print(search_company)
+            CMPN_NM = search_company["CMPN_NM"]
+            WKP_ADRS = search_company["WKP_ADRS"]
+            PK_NM_HASH = search_company["PK_NM_HASH"]
             try:
-                for company in search_list:
-
-                    corp_name = company.find("h2", class_="corp_name")
-                    name = corp_name.find("a")
-                    url = name["href"]
-                    name = name.text.strip()
-
-                    company_pk = url.split("csn=")[-1]
-                    data = {}
-                    print(url)
-                    corp_info = company.find("div", class_="corp_info")
-                    data_list = corp_info.find_all("dl")
-                    address = ""
-                    for val in data_list:
-                        key = val.find("dt").text.strip()
-                        value = re.sub(r" {2,}", "", val.find("dd").text.strip())
-                        if key == "기업주소":
-                            address = value
-                        data[key] = value
-                    Saramin(
-                        name=name, company_pk=company_pk, address=address, data=data
-                    ).save()
+                k = KreditJob.objects.get(company_pk=PK_NM_HASH)
+                print("skip")
                 break
-            except:
-                pass
+                k.name = CMPN_NM
+                k.address = WKP_ADRS
+                k.company_base_content = company_base_content
+                k.company_info_data = company_info_data
+                k.company_jobdam = company_jobdam
+                k.save()
+            except KreditJob.DoesNotExist:
+                company_base_content = json.loads(
+                    requests.get(
+                        f"https://www.wanted.co.kr/api/v1/company_briefs?kreditjob_pk={PK_NM_HASH}",
+                        proxies=proxies,
+                    ).content
+                )
+                # POST
+                # 기업 연봉포함 정보
+                # data
+                data = {"PK_NM_HASH": PK_NM_HASH}
+                company_info_data = json.loads(
+                    requests.post(
+                        "https://www.kreditjob.com/api/company/companyPage",
+                        data=data,
+                        proxies=proxies,
+                    ).content
+                )
+                # POST
+                # 기업 잡담
+                data = {"PK_NM_HASH": ["PK_NM_HASH", PK_NM_HASH]}
+                company_jobdam = json.loads(
+                    requests.post(
+                        "https://www.kreditjob.com/api/jobdom/multiFilter/1/10",
+                        data=data,
+                        proxies=proxies,
+                    ).content
+                )
+                # print("-----------------기업기본정보---------------------")
+                # print(company_base_content)
+                # print("-----------------기업연봉포함정보---------------------")
+                # print(company_info_data)
+                # print("-----------------기업잡담---------------------")
+                # print(company_jobdam)
 
-        page += 1
+                print("-----------------기업기본정보---------------------")
+                print(company_base_content)
+                KreditJob(
+                    name=CMPN_NM,
+                    address=WKP_ADRS,
+                    company_pk=PK_NM_HASH,
+                    company_base_content=company_base_content,
+                    company_info_data=company_info_data,
+                    company_jobdam=company_jobdam,
+                ).save()
+        except:
+            pass
+
+
+def get_company_info(name):
+    get_jobplanet_company(name)
+    get_saramin_company(name)
+    get_kreditjob_company(name)
