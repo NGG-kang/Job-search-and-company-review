1aad10b9645fe1cc82ecb1ecdabb7144831ea2b7
NGG-kang <skarndrkd1222@gmail.com>
2022-03-07 22:25:31 +0900

    Feat: static files change

--- company_review/config/settings/base.py
+++ company_review/config/settings/base.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "django_htmx",
     "django_celery_results",
     "django_celery_beat",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -133,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/staticfiles/")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 
 
 # Default primary key field type
