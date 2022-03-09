bae1906dea660cf624389ec6be4d20b695769563
NGG-kang <skarndrkd1222@gmail.com>
2022-03-09 23:32:35 +0900

    Feat: add company update function

--- company_review/celeries/get_compnay_info.py
+++ company_review/celeries/get_compnay_info.py
@@ -1,3 +1,4 @@
+from ctypes import addressof
 import os
 import sys
 import django
@@ -20,11 +21,7 @@ def process_name(name):
     return name
 
 
-def get_jobplanet_company(name):
-    print(
-        "---------------------------------JOBPLNET----------------------------------------"
-    )
-
+def get_jobplanet_company(name, update=False):
     def find_content(soup):
         company_name = (
             soup.find("div", {"class": "company_info_box"})
@@ -41,40 +38,59 @@ def get_jobplanet_company(name):
             company_info[header.text.strip()] = content.text.strip()
         return (company_name, company_info)
 
-    for _ in range(2):
-        try:
-            search_url = f"https://www.jobplanet.co.kr/autocomplete/autocomplete/suggest.json?term={name}"
-            html = requests.get(search_url, allow_redirects=False, proxies=proxies)
-            companies = json.loads(html.content)["companies"]
-            if html.status_code in (302, 404):
-                break
-            # 업데이트 할거면 풀기
-            for company in companies:
+    def get_company_content(_id):
+        search_url = f"https://www.jobplanet.co.kr/companies/{_id}/landing/"
+        html = requests.get(search_url, allow_redirects=False, proxies=proxies)
+        soup = BeautifulSoup(html.content, "lxml")
+        company_name, company_info = find_content(soup)
+        address = company_info["주소"]
+        _address = address.replace("본사: ", "")
+        _address = address.split(" ")
+        if len(_address) >= 2:
+            _address[0] = _address[0].replace("광역시", "")
+            _address[0] = _address[0].replace(",", "")
+            search_address = f"{_address[0]} {_address[1]}"
+        else:
+            search_address = address
+        return (company_name, company_info, address, search_address)
+
+    print(
+        "---------------------------------JOBPLNET----------------------------------------"
+    )
+    search_url = f"https://www.jobplanet.co.kr/autocomplete/autocomplete/suggest.json?term={name}"
+    html = requests.get(search_url, allow_redirects=False, proxies=proxies)
+    companies = json.loads(html.content)["companies"]
+    if html.status_code not in (302, 404):
+        # 업데이트 할거면 풀기
+        for company in companies:
+            try:
                 name = company["name"]
                 _id = company["id"]
                 try:
                     j = JobPlanet.objects.get(company_pk=_id)
-                    print("skip")
-                    continue
-                    j.name = company_name
-                    j.data = company_info
+                    if not update:
+                        print("skip")
+                        continue
+                    (
+                        company_name,
+                        company_info,
+                        address,
+                        search_address,
+                    ) = get_company_content(_id)
+                    j.name = (process_name(company_name),)
+                    j.company_pk = (_id,)
+                    j.data = (company_info,)
+                    j.address = address
+                    j.search_address = (search_address,)
                     j.save()
+                    print(company_name, "업데이트")
                 except JobPlanet.DoesNotExist:
-                    search_url = f"https://www.jobplanet.co.kr/companies/{_id}/landing/"
-                    html = requests.get(
-                        search_url, allow_redirects=False, proxies=proxies
-                    )
-                    soup = BeautifulSoup(html.content, "lxml")
-                    company_name, company_info = find_content(soup)
-                    address = company_info["주소"]
-                    _address = address.replace("본사: ", "")
-                    _address = address.split(" ")
-                    if len(_address) >= 2:
-                        _address[0] = _address[0].replace("광역시", "")
-                        _address[0] = _address[0].replace(",", "")
-                        search_address = f"{_address[0]} {_address[1]}"
-                    else:
-                        search_address = address
+                    (
+                        company_name,
+                        company_info,
+                        address,
+                        search_address,
+                    ) = get_company_content(_id)
                     JobPlanet(
                         name=process_name(company_name),
                         company_pk=_id,
@@ -83,14 +99,34 @@ def get_jobplanet_company(name):
                         search_address=search_address,
                     ).save()
                     print(company_name, "저장")
-            break
-        except Exception as e:
-            print(e)
+            except:
+                pass
 
 
-def get_saramin_company(name):
+def get_saramin_company(name, update=False):
+    def get_company_content(soup):
+        data = {}
+        corp_info = soup.find("div", class_="corp_info")
+        data_list = corp_info.find_all("dl")
+        address = ""
+        _address = ""
+        for val in data_list:
+            key = val.find("dt").text.strip()
+            value = re.sub(r" {2,}", "", val.find("dd").text.strip())
+            if key == "기업주소":
+                address = value
+                _address = address.split(" ")
+                if len(_address) >= 2:
+                    _address[0] = _address[0].replace("광역시", "")
+                    _address[0] = _address[0].replace(",", "")
+                    _address = f"{_address[0]} {_address[1]}"
+                else:
+                    _address = address
+            data[key] = value
+        return address, _address, data
+
     page = 1
-    for _ in range(2):
+    while True:
         resq = requests.get(
             f"https://www.saramin.co.kr/zf_user/search/company?searchword={name}&page={page}",
             proxies=proxies,
@@ -108,33 +144,25 @@ def get_saramin_company(name):
 
                 company_pk = url.split("csn=")[-1]
                 try:
-                    Saramin.objects.get(company_pk=company_pk)
-                    print("skip")
+                    s = Saramin.objects.get(company_pk=company_pk)
+                    if not update:
+                        print("skip")
+                        continue
+                    address, search_address, data = get_company_content(soup)
+                    s.name = (process_name(name),)
+                    s.company_pk = company_pk
+                    s.address = address
+                    s.data = data
+                    s.search_address = search_address
+                    s.save()
                 except Saramin.DoesNotExist:
-                    data = {}
-                    corp_info = company.find("div", class_="corp_info")
-                    data_list = corp_info.find_all("dl")
-                    address = ""
-                    _address = ""
-                    for val in data_list:
-                        key = val.find("dt").text.strip()
-                        value = re.sub(r" {2,}", "", val.find("dd").text.strip())
-                        if key == "기업주소":
-                            address = value
-                            _address = address.split(" ")
-                            if len(_address) >= 2:
-                                _address[0] = _address[0].replace("광역시", "")
-                                _address[0] = _address[0].replace(",", "")
-                                _address = f"{_address[0]} {_address[1]}"
-                            else:
-                                _address = address
-                        data[key] = value
+                    address, search_address, data = get_company_content(soup)
                     Saramin(
                         name=process_name(name),
                         company_pk=company_pk,
                         address=address,
                         data=data,
-                        search_address=_address,
+                        search_address=search_address,
                     ).save()
                     print(name, "저장")
             except:
@@ -142,7 +170,37 @@ def get_saramin_company(name):
         page += 1
 
 
-def get_kreditjob_company(company):
+def get_kreditjob_company(company, update=False):
+    def get_company_content(PK_NM_HASH):
+        company_base_content = json.loads(
+            requests.get(
+                f"https://www.wanted.co.kr/api/v1/company_briefs?kreditjob_pk={PK_NM_HASH}",
+                proxies=proxies,
+            ).content
+        )
+        # POST
+        # 기업 연봉포함 정보
+        # data
+        data = {"PK_NM_HASH": PK_NM_HASH}
+        company_info_data = json.loads(
+            requests.post(
+                "https://www.kreditjob.com/api/company/companyPage",
+                data=data,
+                proxies=proxies,
+            ).content
+        )
+        # POST
+        # 기업 잡담
+        data = {"PK_NM_HASH": ["PK_NM_HASH", PK_NM_HASH]}
+        company_jobdam = json.loads(
+            requests.post(
+                "https://www.kreditjob.com/api/jobdom/multiFilter/1/10",
+                data=data,
+                proxies=proxies,
+            ).content
+        )
+        return company_base_content, company_info_data, company_jobdam
+
     print(
         "---------------------------------KREDITJOB----------------------------------------"
     )
@@ -163,8 +221,14 @@ def get_kreditjob_company(company):
             PK_NM_HASH = search_company["PK_NM_HASH"]
             try:
                 k = KreditJob.objects.get(company_pk=PK_NM_HASH)
-                print("skip")
-                break
+                if not update:
+                    print("skip")
+                    continue
+                (
+                    company_base_content,
+                    company_info_data,
+                    company_jobdam,
+                ) = get_company_content(PK_NM_HASH)
                 k.name = CMPN_NM
                 k.address = WKP_ADRS
                 k.company_base_content = company_base_content
@@ -172,33 +236,7 @@ def get_kreditjob_company(company):
                 k.company_jobdam = company_jobdam
                 k.save()
             except KreditJob.DoesNotExist:
-                company_base_content = json.loads(
-                    requests.get(
-                        f"https://www.wanted.co.kr/api/v1/company_briefs?kreditjob_pk={PK_NM_HASH}",
-                        proxies=proxies,
-                    ).content
-                )
-                # POST
-                # 기업 연봉포함 정보
-                # data
-                data = {"PK_NM_HASH": PK_NM_HASH}
-                company_info_data = json.loads(
-                    requests.post(
-                        "https://www.kreditjob.com/api/company/companyPage",
-                        data=data,
-                        proxies=proxies,
-                    ).content
-                )
-                # POST
-                # 기업 잡담
-                data = {"PK_NM_HASH": ["PK_NM_HASH", PK_NM_HASH]}
-                company_jobdam = json.loads(
-                    requests.post(
-                        "https://www.kreditjob.com/api/jobdom/multiFilter/1/10",
-                        data=data,
-                        proxies=proxies,
-                    ).content
-                )
+
                 # print("-----------------기업기본정보---------------------")
                 # print(company_base_content)
                 # print("-----------------기업연봉포함정보---------------------")
