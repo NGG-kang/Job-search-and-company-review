372b25847cb3bc8fdc3ba20a1528ca17afd8f876
NGG-kang <skarndrkd1222@gmail.com>
2022-03-10 23:10:04 +0900

    Feat: cache set all company list

--- company_review/crawling/tests.py
+++ company_review/crawling/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from crawling.models import KreditJob, Saramin, JobPlanet
+from django.core.cache import cache
+
+
+def get_all_company_name():
+    k = [i.name for i in KreditJob.objects.all().only("name").distinct("name")]
+    s = [i.name for i in Saramin.objects.all().only("name").distinct("name")]
+    j = [i.name for i in JobPlanet.objects.all().only("name").distinct("name")]
+    k.extend(s)
+    k.extend(j)
+    k = set(k)
+    k = list(k)
+    cache.set("company_list", k, None)
