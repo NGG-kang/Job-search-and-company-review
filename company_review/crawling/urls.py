0cf6cbecb287d2509963574c0e3dc06a2ecb3adf
NGG-kang <skarndrkd1222@gmail.com>
2022-03-24 21:59:06 +0900

    Feat: add search beat view

--- company_review/crawling/urls.py
+++ company_review/crawling/urls.py
@@ -4,4 +4,5 @@ from crawling import views
 urlpatterns = [
     path("", views.search_result, name="search"),
     path("update_company/", views.update_company, name="update_company"),
+    path("add-search-info/", views.add_search_cron_beat, name="add-search-info"),
 ]
--- company_review/crawling/views.py
+++ company_review/crawling/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
-from .models import KreditJob, JobPlanet, Saramin, SearchResult
+from .models import KreditJob, JobPlanet, Saramin
 from celeries.celery import get_company_info
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from django.core.cache import cache
 
 # Create your views here.
@@ -30,10 +31,9 @@ def update_company(request, **kwargs):
     if request.htmx:
         if request.POST:
             company = request.POST.get("company")
-            # get_company_info.delay(company, True)
+            get_company_info.delay(company, True)
             return JsonResponse({"message": "success"})
     return HttpResponseBadRequest("using method post")
-
 
 
 def search_company(request, **kwargs):
@@ -45,3 +45,21 @@ def search_company(request, **kwargs):
         k = KreditJob.objects.filter(name=company).values()
         context = {"saramin": s, "jobplanet": j, "kreditjob": k}
     return render(request=request, template_name=template, context=context)
+
+
+def add_search_cron_beat(request):
+    if request.htmx:
+        if request.POST:
+            try:
+                company = request.POST.get("company")
+                interval = IntervalSchedule.objects.get(id=1)
+                PeriodicTask.objects.create(
+                    name=company,
+                    task="crawling.search_jobs.search.search_and_save",
+                    enabled=True,
+                    interval=interval
+                )
+                return JsonResponse({"message": "success"})
+            except Exception:
+                return HttpResponseBadRequest("Already save company")            
+    return HttpResponseBadRequest("using method post")
