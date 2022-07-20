0ac6da49855055d69836ea2893086a7896ddd774
NGG-kang <skarndrkd1222@gmail.com>
2022-07-20 22:23:59 +0900

    Chore; decrese worker 2

--- company_review/entrypoint/entrypoint.prod.sh
+++ company_review/entrypoint/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-celery -A celeries.celery worker -B -l INFO --concurrency=4 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A celeries.celery worker -B -l INFO --concurrency=2 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 python manage.py collectstatic --noinput
 
