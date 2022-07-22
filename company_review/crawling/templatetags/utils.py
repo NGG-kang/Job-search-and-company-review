7e0f65d0a3cce5dbe78ce0c7077729b5da219407
NGG-kang <skarndrkd1222@gmail.com>
2022-07-23 01:18:11 +0900

    Feat: 템플릿 업데이트
    1. 모바일 반응형
    2. 기업정보 리스트
    3. 크레딧잡 정보 추가

--- company_review/crawling/templatetags/utils.py
+++ company_review/crawling/templatetags/utils.py
@@ -6,3 +6,17 @@ register = template.Library()
 @register.filter
 def get_type(value):
     return type(value).__name__
+
+
+@register.filter
+def percentage(value):
+    if value:
+        return str(round((float(value) * 100), 0)) + "%"
+    return "None"
+
+
+@register.filter
+def is_bool(value):
+    if value == "true":
+        return True
+    return False
