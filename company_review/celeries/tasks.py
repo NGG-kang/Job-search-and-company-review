5c8c1befe800b79d31d88935e7a5964fbcc73a84
NGG-kang <skarndrkd1222@gmail.com>
2022-07-30 01:03:46 +0900

    Feat: task 분리

--- company_review/celeries/tasks.py
+++ company_review/celeries/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+import traceback
 from celeries.get_compnay_info import (
     get_kreditjob_company,
     get_saramin_company,
@@ -7,11 +8,18 @@ from celeries.get_compnay_info import (
 
 
 @shared_task
-def get_company_info(name, update=False):
-    print("시작: " + name)
-    print("---------------------------------잡플래닛 시작: " + name + "----------------------------------------")
-    get_jobplanet_company(name, update)
-    print("---------------------------------사람인 시작: " + name + "----------------------------------------")
+def get_saramin_info(name, update=False):
     get_saramin_company(name, update)
-    print("---------------------------------크래딧잡 시작: " + name + "----------------------------------------")
-    get_kreditjob_company(name, update)
+
+
+@shared_task
+def get_jobplanet_info(name, update=False):
+    get_jobplanet_company(name, update)
+
+
+@shared_task
+def get_kreditjob_info(name, update=False):
+    try:
+        get_kreditjob_company(name, update)
+    except:
+        print(traceback.print_exc())
