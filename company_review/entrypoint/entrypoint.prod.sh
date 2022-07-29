d6dc3b7641658dc2980596a364dc5e7551e5e148
NGG-kang <skarndrkd1222@gmail.com>
2022-07-30 01:09:31 +0900

    Feat: celery run add queue

--- company_review/entrypoint/entrypoint.prod.sh
+++ company_review/entrypoint/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-celery -A celeries.celery worker -B -l INFO --concurrency=3 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A celeries.celery worker -B -l INFO -Q search,saramin,kreditjob,jobplanet --concurrency=3 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 python manage.py collectstatic --noinput
 
