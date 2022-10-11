bb77727aac93263413e5440051a9bcab2aae664d
NGG-kang <skarndrkd1222@gmail.com>
2022-10-11 19:41:22 +0900

    Feat: add wanted crawling

--- company_review/crawling/search_jobs/search.py
+++ company_review/crawling/search_jobs/search.py
@@ -1,12 +1,6 @@
-import os
-import sys
-import django
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(os.path.dirname(BASE_DIR))
 from crawling.search_jobs.saramin import get_saramin_search
 from crawling.search_jobs.jobkorea import get_jobkorea_search
+from crawling.search_jobs.wanted import get_wanted_search
 from celery import shared_task
 from crawling.models import JobPlanet, KreditJob, Saramin, SearchResult
 from itertools import chain
@@ -16,87 +10,100 @@ from config.utils import process_address
 from django.utils import timezone
 
 
-def search_and_save_row(q, celery=False):
+@shared_task
+def search_and_save(q, wanted):
     saramin, saramin_length = get_saramin_search(q)
     jobkorea, jobkorea_length = get_jobkorea_search(q)
-    all_jobs = list(chain(saramin, jobkorea))
-    if celery:
-        try:
-            names = [i["name"] for i in list(chain(saramin, jobkorea))]
-            s = Saramin.objects.filter(name__in=names).only("name", "search_address", "company_pk", "data")
-            k = KreditJob.objects.filter(name__in=names).only(
-                    "name",
-                    "search_address",
-                    "company_pk",
-                    "company_base_content",
-                    # "company_info_data",
-                    "company_jobdam",
-                )
-            j = JobPlanet.objects.filter(name__in=names).only("name", "search_address", "company_pk", "data")
+    wanted, wanted_length = get_wanted_search(wanted)
+    all_jobs = list(chain(saramin, jobkorea, wanted))
+    
+    print("run search company matching")
+    try:
+        names = [i["name"] for i in list(chain(saramin, jobkorea, wanted))]
+        s = Saramin.objects.filter(name__in=names).only("name", "search_address", "company_pk", "data")
+        k = KreditJob.objects.filter(name__in=names).only(
+                "name",
+                "search_address",
+                "company_pk",
+                "company_base_content",
+                # "company_info_data",
+                "company_jobdam",
+            )
+        j = JobPlanet.objects.filter(name__in=names).only("name", "search_address", "company_pk", "data")
 
-            def get_model_dict(objs):
-                if objs:
-                    obj = dict()
-                    for i in objs:
-                        if i["name"] in obj:
-                            obj[i["name"]].append(i)
-                        else:
-                            obj[i["name"]] = [i]
-                    return obj
+        def get_model_dict(objs):
+            if objs:
+                obj = dict()
+                for i in objs:
+                    if i["name"] in obj:
+                        obj[i["name"]].append(i)
+                    else:
+                        obj[i["name"]] = [i]
+                return obj
+            return None
+
+        saramin_object = get_model_dict(
+            s.values("name", "search_address", "company_pk", "data")
+        )
+        jobplanet_object = get_model_dict(
+            j.values("name", "search_address", "company_pk", "data")
+        )
+        kreditjob_object = get_model_dict(
+            k.values(
+                "name",
+                "search_address",
+                "company_pk",
+                "company_base_content",
+                # "company_info_data",
+                "company_jobdam",
+            )
+        )
+
+        def get_company_info_dict(job, value_dict):
+            if value_dict and job["name"] in value_dict:
+                for j in value_dict[job["name"]]:
+                    if process_address(job["work_place"]) == j["search_address"]:
+                        return j
+                return [val for val in value_dict[job["name"]]]
+            else:
                 return None
 
-            saramin_object = get_model_dict(
-                s.values("name", "search_address", "company_pk", "data")
-            )
-            jobplanet_object = get_model_dict(
-                j.values("name", "search_address", "company_pk", "data")
-            )
-            kreditjob_object = get_model_dict(
-                k.values(
-                    "name",
-                    "search_address",
-                    "company_pk",
-                    "company_base_content",
-                    # "company_info_data",
-                    "company_jobdam",
-                )
-            )
+        saramin_list = list()
+        jobkorea_list = list()
+        wanted_list = list()
+        for index, job in enumerate(all_jobs):
+            company_info_list = list()
+            company_info_list.append(get_company_info_dict(job, saramin_object))
+            company_info_list.append(get_company_info_dict(job, jobplanet_object))
+            company_info_list.append(get_company_info_dict(job, kreditjob_object))
+            job["objs"] = company_info_list
+            if index < saramin_length:
+                saramin_list.append(job)
+                continue
 
-            def get_company_info_dict(job, value_dict):
-                if value_dict and job["name"] in value_dict:
-                    for j in value_dict[job["name"]]:
-                        if process_address(job["work_place"]) == j["search_address"]:
-                            return j
-                    return [val for val in value_dict[job["name"]]]
-                else:
-                    return None
-
-            saramin_list = list()
-            jobkorea_list = list()
-            for index, job in enumerate(all_jobs):
-                company_info_list = list()
-                company_info_list.append(get_company_info_dict(job, saramin_object))
-                company_info_list.append(get_company_info_dict(job, jobplanet_object))
-                company_info_list.append(get_company_info_dict(job, kreditjob_object))
-                job["objs"] = company_info_list
-                if index < saramin_length:
-                    saramin_list.append(job)
-                    continue
-                if index < saramin_length + jobkorea_length:
-                    jobkorea_list.append(job)
-                    continue
-            all_jobs = {'saramin': saramin_list, 'jobkorea': jobkorea_list}
-            all_jobs['time'] = timezone.now()
-            cache.set(q, all_jobs, None)
-        except:
-            print(print_exc())
+            if index < saramin_length + jobkorea_length:
+                jobkorea_list.append(job)
+                continue
+            
+            if index < saramin_length + jobkorea_length + wanted_length:
+                wanted_list.append(job)
+                continue
+        all_jobs = {'saramin': saramin_list, 'jobkorea': jobkorea_list, "wanted": wanted_list}
+        all_jobs['time'] = timezone.now()
+        print("set cache new")
+        cache.set(q, all_jobs, None)
+    except:
+        print(print_exc())
+    print("serch job end")
     return all_jobs
 
 
-@shared_task
-def search_and_save(q, celery=False):
-    search_and_save_row(q, celery)
-
-
 if __name__ == "__main__":
-    search_and_save_row("java", celery=True)
+    import os
+    import sys
+    import django
+    from pathlib import Path
+
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    sys.path.append(os.path.dirname(BASE_DIR))
+    search_and_save("java", celery=True)
--- company_review/crawling/views.py
+++ company_review/crawling/views.py
@@ -6,7 +6,33 @@ from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from django.core.cache import cache
 from django.contrib import messages
 import traceback
+import requests
+import json
 
+with open('./crawling/search_jobs/wanted_category.json', 'r') as f:
+    wanted_category = json.load(f)
+
+def init_context(request):
+    context = dict()
+    q = request.GET.get("q", "")
+    data = cache.get(q)
+    if q:
+        if data:
+            context = {"context": data}
+        try:
+            PeriodicTask.objects.get(name=q)
+            data = True
+        except PeriodicTask.DoesNotExist:
+            wanted_res = requests.get(f"https://www.wanted.co.kr/api/v4/tags/autocomplete?kinds=SKILL&keyword={q}")
+            data = False
+            if wanted_res.json()['results']:
+                context['wanted_q'] = wanted_res.json()['results']
+            context['wanted_category'] = wanted_category
+    context["q"] = q     
+    context["data"] = data
+    context["tasks"] = PeriodicTask.objects.all().values("name", "task", "last_run_at", "interval__period", "interval__every", )
+        
+    return context
 
 def search_result(request, **kwargs):
     context = {}    
@@ -14,19 +40,8 @@ def search_result(request, **kwargs):
         template = "result.html"
     else:
         template = "index.html"
-
-    q = request.GET.get("q")
-    if q:
-        data = cache.get(q)
-        if data:
-            context = {"context": data}
-    try:
-        PeriodicTask.objects.get(name=q)
-        data = True
-    except PeriodicTask.DoesNotExist:
-        data = False
-    context["data"] = data
-    context["tasks"] = PeriodicTask.objects.all().values("name", "task", "last_run_at", "interval__period", "interval__every", )
+    
+    context = init_context(request)
     return render(request=request, template_name=template, context=context)
 
 
@@ -39,36 +54,32 @@ def update_company(request, **kwargs):
     return HttpResponseBadRequest("using method post")
 
 
-def search_company(request, **kwargs):
-    template = "search_company.html"
-    company = request.GET.get("company")
-    if company:
-        s = Saramin.objects.filter(name=company).values()
-        j = JobPlanet.objects.filter(name=company).values()
-        k = KreditJob.objects.filter(name=company).values()
-        context = {"saramin": s, "jobplanet": j, "kreditjob": k}
-    return render(request=request, template_name=template, context=context)
-
-
 def add_search_cron_beat(request):
+    context = init_context(request)
     if request.htmx:
         if request.POST:
             try:
-                company = request.POST.get("company")
-                if not company:
+                search_q = request.POST.get("search_q", "")
+                wanted_q = request.POST.get("wanted_q", "")
+                wanted_category = request.POST.get("wanted_category", "")
+                wanted = {"wanted_category": "", "wanted_q": ""}
+                if not search_q:
                     return HttpResponseBadRequest("Need company data")
-                interval = IntervalSchedule.objects.get_or_create(every=1, period='minutes')[0]
+                if wanted_q:
+                    wanted = {"wanted_category": wanted_category, "wanted_q": wanted_q}
+                    
+                interval = IntervalSchedule.objects.get_or_create(every=30, period='minutes')[0]
                 PeriodicTask.objects.create(
-                    name=company,
+                    name=search_q,
                     task="crawling.search_jobs.search.search_and_save",
                     enabled=True,
                     interval=interval,
-                    args=f'["{company}", "True"]',
+                    kwargs=json.dumps({"q": search_q, "wanted": wanted}),
                     priority=0,
                     queue='search'
                 )
-                messages.add_message(request, messages.INFO, 'Hello world.')
-                return render(request=request, template_name="result.html")
+                messages.add_message(request, messages.SUCCESS, 'Save Success')
+                return render(request=request, template_name="result.html", context=context)
             except Exception as e:
                 print(traceback.print_exc())
                 print(e)
