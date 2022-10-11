043ca5832864e193deadcdfbdb8a44cf0cfa0504
NGG-kang <skarndrkd1222@gmail.com>
2022-10-11 20:15:37 +0900

    Feat: add django honeypot admin

--- company_review/config/urls.py
+++ company_review/config/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('secret/', admin.site.urls),
     path("", include("crawling.urls")),
 ]
 
