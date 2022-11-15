15f5957a9e1603fe6601abb1ea6b272c8f03eb90
NGG-kang <skarndrkd1222@gmail.com>
2022-11-15 22:56:29 +0900

    Chore: start celery delete

--- company_review/entrypoint/entrypoint.prod.sh
+++ company_review/entrypoint/entrypoint.prod.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-celery -A celeries.celery worker -B -l INFO -Q search,saramin,kreditjob,jobplanet --concurrency=3 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-
 python manage.py collectstatic --noinput
 
 exec "$@"
