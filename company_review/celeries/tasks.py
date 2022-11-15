454d965b69aaef4a8302968fb7b6a711e44b210f
NGG-kang <skarndrkd1222@gmail.com>
2022-11-15 22:59:25 +0900

    Feat:  add task interval task

--- company_review/celeries/tasks.py
+++ company_review/celeries/tasks.py
@@ -5,6 +5,8 @@ from celeries.get_compnay_info import (
     get_saramin_company,
     get_jobplanet_company,
 )
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+from django.utils import timezone
 
 
 @shared_task
@@ -23,3 +25,10 @@ def get_kreditjob_info(name, update=False):
         get_kreditjob_company(name, update)
     except:
         print(traceback.print_exc())
+
+@shared_task
+def change_task_interval():
+    tasks = PeriodicTask.objects.exclude(name="celery.backend_cleanup")
+    interval = IntervalSchedule.objects.get_or_create(every=6, period='hours')[0]
+    tasks.interval = interval
+    tasks.save()
